564981bbcad62ff5dce505781ece6eb2f1cb3993
Abraham Ubuntu <abcod98@gmail.com>
2022-09-15 20:21:12 -0600

    Caracteristicas de ES11

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,10 @@
+const users = {
+  abcod: {
+    country: 'GT'
+  },
+  dev: {
+    country: 'CO'
+  }
+}
+
+console.log(users?.user1?.country);
---
+++ src/es11/01-bigint.js
@@ -0,0 +1,4 @@
+const aBigNumber = 1980509823958523n;
+const anotherBigNumber = BigInt(1980509823958523);
+console.log(aBigNumber);
+console.log(anotherBigNumber);
